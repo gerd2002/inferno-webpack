0222fcbddde34c2309e1c50442ecfb42c1ac96ef
Gerd Sattler <gerd2002@outlook.com>
2018-02-15 14:06:58 +0800

    added hot style reload in development

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,9 +1,14 @@
 const merge = require('webpack-merge');
+const webpack = require('webpack');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: 'dist/'
-    }
+        contentBase: 'dist/',
+        hot: true
+    },
+    plugins: [
+    	new webpack.HotModuleReplacementPlugin()
+    ]
 });
